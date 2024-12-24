169a1b327dafe6bae861f499b21e30c0e1ff4b91
sidmohanty11 <sidmohanty11@gmail.com>
2024-12-24 14:20:07 +0530

    try

--- src/hello.ts
+++ src/hello.ts
@@ -1,5 +1,8 @@
-const hello = () => {
-  console.log("hello world");
-};
+// Force foreground-scripts during installation
+process.env.npm_config_foreground_scripts = "true";
 
-hello();
+process.env.npm_config_loglevel = "info";
+
+console.log("\x1b[36m%s\x1b[0m", "----------------------------------------");
+console.log("\x1b[32m%s\x1b[0m", "Hello, world!");
+console.log("\x1b[36m%s\x1b[0m", "----------------------------------------");
